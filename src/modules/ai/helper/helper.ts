77d9355db68b49859a64d350ec309e625de87d6b
Lukas Olsen <61273323+lukasolsen@users.noreply.github.com>
2023-08-13 19:44:43 +0200

    Removed unused dependency

--- src/modules/ai/helper/helper.ts
+++ src/modules/ai/helper/helper.ts
@@ -1,6 +1,4 @@
 import {HfInference} from '@huggingface/inference';
-import * as tf from '@tensorflow/tfjs';
-import '@tensorflow/tfjs-layers';
 
 class Helper {
   static instance: Helper;
@@ -84,100 +82,6 @@ class Helper {
         reject(e);
       });
     }
-  }
-}
-
-/**
- * @deprecated
- */
-class HelperBeta {
-  static instance: Helper;
-  private model: Promise<tf.LayersModel>;
-  wordEmbeddings: {
-    Uncaught: number[]; // Embedding vector for 'Uncaught'
-    'ReferenceError:': number[]; // Embedding vector for 'ReferenceError:'
-    ms: number[];
-  };
-
-  constructor() {
-    this.wordEmbeddings = {
-      Uncaught: [0.1, 0.2, 0.3], // Embedding vector for 'Uncaught'
-      'ReferenceError:': [0.4, 0.5, 0.6], // Embedding vector for 'ReferenceError:'
-      ms: [0.7, 0.8, 0.9], // Embedding vector for 'ms'
-    };
-    try {
-      this.model = this.loadModel();
-    } catch (e) {
-      console.trace('Failed to load the model.');
-    }
-    //this.inference = new HfInference(huggingFaceToken);
-  }
-
-  private async loadModel() {
-    /*const model = await tf.loadLayersModel(
-      'https://storage.googleapis.com/tfjs-models/tfjs/mnist_transfer_cnn_v1/model.json'
-    );*/
-    const curLoc =
-      typeof module !== 'undefined' && module.exports ? __dirname : '';
-    const model = await tf.loadLayersModel(
-      'file://' + curLoc + '../models/bart-large-cnn/tfjs_model/model.json'
-    );
-    return model;
-  }
-
-  static getInstance(huggingFaceToken: string): Helper {
-    if (!this.instance) {
-      this.instance = new Helper(huggingFaceToken);
-    }
-    return this.instance;
-  }
-
-  async summarizeMessage(message: string): Promise<string> {
-    const summary = await this.generateSummary(message);
-    console.log(summary);
-    return await summary; // Return the summary_text from the output
-  }
-
-  async generateSummary(inputText: string): Promise<any> {
-    if (inputText === '') {
-      return;
-    }
-
-    const processedInput = await this.preprocessInput(inputText);
-    const predictTensor = (await this.model).predict(
-      tf.expandDims(processedInput, 0) // Add an additional dimension for batch size
-    ) as tf.Tensor;
-    const output = await this.postProcessPrediction(predictTensor);
-    return output;
-  }
-
-  async preprocessInput(inputText) {
-    // Tokenize and preprocess the input text
-    // You might need to adjust this based on the specifics of your model
-    const tokens = inputText.split(' '); // Split text into tokens
-    // Convert tokens to token embeddings (embeddings can be obtained using word embeddings models)
-    const embeddings = await this.getEmbeddingsForTokens(tokens);
-    const inputTensor = tf.tensor(embeddings); // Convert embeddings to tensor
-    return inputTensor;
-  }
-
-  async postProcessPrediction(predictionTensor) {
-    // Convert the prediction tensor to human-readable output
-    // You might need to adjust this based on your model's output format
-    const prediction = await predictionTensor.array();
-    const outputText = prediction[0].join(' '); // Convert tokens to text
-    return outputText;
-  }
-
-  async getEmbeddingsForTokens(tokens) {
-    const embeddings = [];
-
-    for (const token of tokens) {
-      const embedding = this.wordEmbeddings[token] || [0, 0, 0];
-      embeddings.push(embedding);
-    }
-
-    return embeddings;
   }
 }
 export {Helper};
